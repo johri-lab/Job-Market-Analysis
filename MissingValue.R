83019452f1ddbf36783a56794c6cb2fc636e60cc
Shikhar Johri <shikharjohri123@gmail.com>
2023-11-30 23:43:44 -0500

    updated missing value analysis graph rendering

--- MissingValue.R
+++ MissingValue.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 
-folder_path <- "C:\\Users\\A-Team\\Documents\\MSDS\\EDAV\\Assignments\\final project\\final data"
+folder_path <- "./data"
 
 csv_files <- list.files(folder_path, pattern = "\\.csv$", full.names = TRUE)
 
@@ -13,9 +13,6 @@ for (file in csv_files) {
     summarise_all(~ mean(is.na(.)) * 100) %>%
     gather(key = "variable", value = "missing_percentage") %>%
     arrange(desc(missing_percentage))
-
-  print(csv_file_name)
-  print(missing_summary)
 
   missing_value_graph <- ggplot(missing_summary, aes(x = reorder(variable, -missing_percentage), y = missing_percentage)) +
     geom_bar(stat = "identity", fill = "skyblue") +

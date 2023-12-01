b8d0d614395ea7666b114b8a164a731cf28ad663
Shikhar Johri <shikharjohri123@gmail.com>
2023-12-01 10:25:29 -0500

    updated research plan and render

--- MissingValue.R
+++ MissingValue.R
@@ -16,13 +16,15 @@ for (file in csv_files) {
     arrange(desc(missing_percentage))
 
   missing_value_graph <- ggplot(missing_summary, aes(x = reorder(variable, -missing_percentage), y = missing_percentage)) +
-    geom_bar(stat = "identity", fill = "skyblue") +
+    geom_bar(stat = "identity", fill = "skyblue", width=0.5) +
     geom_text(data = subset(missing_summary, missing_percentage != 0),
               aes(label = round(missing_percentage, 1)), vjust = -0.5, size = 3) +
     labs(title = paste("Missing Value Summary (", csv_file_name,")"),
          x = "Variables",
          y = "missing Values (%)")+
-    theme(axis.text.x = element_text(angle = 45, hjust = 1))
+    theme(axis.text.x = element_text(angle = 45, hjust = 1))+
+    expand_limits(x = 0, y = 110)+
+    scale_y_continuous(expand =c(0,0))
 
   print(missing_value_graph )
 }

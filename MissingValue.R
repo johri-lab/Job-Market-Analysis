01c67eb2725b00c2e92ecd5b2f6084d60438627b
Shikhar Johri <shikharjohri123@gmail.com>
2023-12-15 01:19:18 -0500

    Rendered book

--- MissingValue.R
+++ MissingValue.R
@@ -36,3 +36,241 @@ missing_value_graph <- ggplot(combined_missing_summary, aes(x = reorder(variable
   facet_wrap(~csv_file_name, scales = "free", ncol=2)
 
 print(missing_value_graph)
+
+
+# missing value analysis
+library(redav)
+industry_df <- read.csv('./data/industry.csv')
+plot_missing(industry_df)
+
+
+
+
+
+# plot analysis
+
+
+suppressPackageStartupMessages({
+  library(dplyr)
+  library(ggplot2)
+  library(tidyverse)
+  library(gridExtra)
+  library(plotly)
+  library(janitor)
+})
+
+# Specify the folder path
+folder_path <- "./data/"
+
+folder_path_other <- './data_other/'
+
+
+# Read CSV files with full path
+hires <- read.csv(paste0(folder_path, "Hires.csv"))
+layoffs_discharges <- read.csv(paste0(folder_path, "LayoffsDischarges.csv"))
+industry <- read.csv(paste0(folder_path, "industry.csv"))
+unemployed_per_job_opening_ratio <- read.csv(paste0(folder_path, "UnemployedPerJobOpeningRatio.csv"))
+total_separations <- read.csv(paste0(folder_path, "TotalSeperations.csv"))
+quits <- read.csv(paste0(folder_path, "Quits.csv"))
+job_openings <- read.csv(paste0(folder_path, "JobOpenings.csv"))
+series <- read.csv(paste0(folder_path, "series.csv"))
+
+all_item <- read.csv(paste0(folder_path_other, "AllItems.csv"))
+dataelement <- read.csv(paste0(folder_path_other, "dataelement.csv"))
+sizeclass <- read.csv(paste0(folder_path_other, "sizeclass.csv"))
+state <- read.csv(paste0(folder_path_other, "state.csv"))
+
+
+all_item <- all_item %>%
+  mutate(
+    value = as.numeric(value),
+    series_id = str_trim(series_id),
+    date = as.Date(paste(substr(period,2,3),"01",year,sep="/"),"%m/%d/%Y")
+  )
+
+series<-series %>%
+  clean_names %>%
+  mutate_all(str_trim)
+
+dataelement<-dataelement %>%
+  clean_names %>%
+  mutate_all(str_trim)
+
+sizeclass<-sizeclass %>%
+  clean_names %>%
+  mutate_all(str_trim)
+
+industry<-industry %>%
+  clean_names %>%
+  mutate_all(str_trim)
+
+state<-state %>%
+  clean_names %>%
+  mutate_all(str_trim)
+
+jolts <- all_item %>%
+  inner_join(series, by = c("series_id")) %>%
+  inner_join(dataelement, by = c("dataelement_code"),suffix = c(".series", ".data_element")) %>%
+  inner_join(industry, by = c("industry_code"),suffix = c(".x", ".industry_code")) %>%
+  inner_join(sizeclass, by = "sizeclass_code") %>%
+  inner_join(state, by="state_code")
+
+# write.csv(jolts, "combined_jolts.csv", row.names = FALSE)
+
+# a <- jolts %>%
+#   filter(period != "M13", seasonal == "S", ratelevel_code == "L") %>%
+#   filter(display_level.industry_code == 2) %>%
+#   filter(dataelement_text %in% c("Job openings", "Hires")) %>%
+#   group_by(industry_text, dataelement_text, year = lubridate::year(date)) %>%
+#   summarise(mean_value = mean(value, na.rm = TRUE)) %>%
+#   pivot_wider(names_from = dataelement_text, values_from = mean_value)
+#
+#
+# write.csv(a, "hires_openings_jolts.csv", row.names = FALSE)
+
+
+jolts <- jolts %>%
+  mutate(
+    industry_text = case_when(
+      industry_text == "Professional and business services" ~ "Business services",
+      industry_text == "Education and health services" ~ "Education & health",
+      industry_text == "Leisure and hospitality" ~ "Leisure & hospitality",
+      TRUE ~ industry_text
+    )
+  )
+
+
+
+a <- jolts %>%
+  filter(period != "M13", seasonal == "S", ratelevel_code == "L") %>%
+  filter(display_level.industry_code == 2) %>%
+  filter(dataelement_text %in% c("Job openings", "Hires")) %>%
+  group_by(industry_text, dataelement_text, year = lubridate::year(date)) %>%
+  summarise(mean_value = mean(value, na.rm = TRUE)) %>%
+  pivot_wider(names_from = dataelement_text, values_from = mean_value)jolts %>% filter(period != "M13", seasonal == "S", ratelevel_code == "L") %>%
+  filter(display_level.industry_code == 2) %>%
+  filter(dataelement_text != "Layoffs and discharges", dataelement_text != "Other separations") %>%
+  filter(date == max(date)) %>%
+  ggplot(aes(industry_text, value)) + geom_bar(stat="identity") + facet_wrap(~ dataelement_text) +
+  coord_flip()
+
+a <- jolts %>% filter(period != "M13", seasonal == "S", ratelevel_code == "R") %>%
+  filter(display_level.industry_code == 2) %>%
+  filter(dataelement_text == "Quits" | dataelement_text == "Job openings") %>%
+  filter(year == 2019 | year == 2022) %>%
+  pivot_wider(id_cols = c("date","industry_text"), names_from = dataelement_text, values_from = value) %>%
+  mutate(year = factor(year(date)))
+
+ggplot(a, aes(Quits, `Job openings`, color = year)) + geom_point() + theme_classic() + facet_wrap(~ industry_text)
+
+
+
+
+a <- jolts %>%
+  filter(period != "M13", seasonal == "S", ratelevel_code == "L") %>%
+  filter(display_level.industry_code == 2) %>%
+  filter(dataelement_text == "Job openings") %>%
+  group_by(industry_text, year = lubridate::year(date)) %>%
+  summarise(mean_job_openings = mean(value, na.rm = TRUE))
+
+plot2 <- ggplot(a, aes(x = year, y = mean_job_openings)) +
+  geom_line() +
+  geom_point() +
+  facet_wrap(~ industry_text) +
+  labs(title = "Mean Job Openings per Year", x = "Year", y = "Mean Job Openings")
+
+plot2
+
+
+# Mean Job Openings and Hires per Year
+a <- jolts %>%
+  filter(period != "M13", seasonal == "S", ratelevel_code == "L") %>%
+  filter(display_level.industry_code == 2) %>%
+  filter(dataelement_text %in% c("Job openings", "Hires")) %>%
+  group_by(industry_text, dataelement_text, year = lubridate::year(date)) %>%
+  summarise(mean_value = mean(value, na.rm = TRUE)) %>%
+  pivot_wider(names_from = dataelement_text, values_from = mean_value)
+
+plot2 <- ggplot(a, aes(x = year)) +
+  geom_line(aes(y = `Job openings`, color = "Job Openings")) +
+  geom_point(aes(y = `Job openings`, color = "Job Openings")) +
+  geom_line(aes(y = Hires, color = "Hires")) +
+  geom_point(aes(y = Hires, color = "Hires")) +
+  facet_wrap(~ industry_text) +
+  labs(title = "Mean Job Openings and Hires per Year", x = "Year", y = "Mean Value", color = "Metric")
+
+plotly1 <- ggplotly(plot2)
+plotly1
+
+
+
+
+
+# Overall current market analysis
+jolts_filtered <- jolts %>%
+  filter(period != "M13", seasonal == "S", ratelevel_code == "L") %>%
+  filter(display_level.industry_code == 2) %>%
+  filter(dataelement_text != "Layoffs and discharges", dataelement_text != "Other separations") %>%
+  filter(date == max(date))
+
+plot1 <- jolts_filtered %>%
+  ggplot(aes(industry_text, value)) +
+  geom_bar(stat = "identity") +
+  coord_flip() +
+  facet_wrap(~ dataelement_text) +
+  labs(title = "Industry Data (Current 2023 Market)", x = "Industry", y = "Value")
+
+plotly1 <- ggplotly(plot1)
+plotly1
+
+
+
+
+
+
+
+
+
+
+
+# Job openings vs Hires
+
+a <- jolts %>%
+  filter(period != "M13", seasonal == "S", ratelevel_code == "R") %>%
+  filter(display_level.industry_code == 2) %>%
+  filter(dataelement_text == "Hires" | dataelement_text == "Job openings") %>%
+  filter(year> 2021) %>%
+  pivot_wider(id_cols = c("date","industry_text"), names_from = dataelement_text, values_from = value) %>%
+  mutate(year = factor(year(date)))
+
+plot2 <- ggplot(a, aes(Hires, `Job openings`, color = year)) +
+  geom_point() +
+  theme_classic() +
+  facet_wrap(~ industry_text) +
+  labs(title = "Job Openings vs Hires", x = "Hires", y = "Job Openings")
+
+plotly2 <- ggplotly(plot2)
+plotly2
+
+
+
+# Reasons for leaving
+a <- jolts %>%
+  filter(period != "M13", seasonal == "S", ratelevel_code == "R") %>%
+  filter(display_level.industry_code == 2) %>%
+  filter(dataelement_text == "Quits" | dataelement_text == "Layoffs and discharges") %>%
+  filter(year == 2019 | year == 2022) %>%
+  pivot_wider(id_cols = c("date","industry_text"), names_from = dataelement_text, values_from = value) %>%
+  mutate(year = factor(year(date)))
+
+plot2 <- ggplot(a, aes(Quits, `Layoffs and discharges`, color = year)) +
+  geom_point() +
+  theme_classic() +
+  facet_wrap(~ industry_text) +
+  labs(title = "Layoffs and discharges vs Quits", x = "Quits", y = "Layoffs and discharges")
+
+plotly2 <- ggplotly(plot2)
+plotly2
+
+
+
--- docs/scripts/myscript.js
+++ docs/scripts/myscript.js
@@ -1,1 +1,185 @@
-// add your JavaScript/D3 to this file
+// Function to convert row data
+function rowConverter(row) {
+  return {
+    industry_text: row.industry_text,
+    year: +row.year,
+    Hires: +row.Hires,
+    JobOpenings: +row["Job openings"]
+  };
+}
+
+// Function to populate dropdown options
+function populateDropdownOptions(data) {
+  const industryDropdown = d3.select("#industryDropdown");
+
+  // Get unique industry values
+  const uniqueIndustries = Array.from(new Set(data.map(d => d.industry_text)));
+
+  // Populate dropdown with options
+  industryDropdown
+    .selectAll("option")
+    .data(uniqueIndustries)
+    .enter()
+    .append("option")
+    .text(d => d)
+    .attr("value", d => d);
+}
+
+// Function to update the chart
+function updateChart(data) {
+  // Check if data is loaded
+  if (!data || data.length === 0) {
+    console.warn("No data available.");
+    return;
+  }
+
+  // Select dropdown and buttons
+  const industryDropdown = d3.select("#industryDropdown");
+  const hiresButton = d3.select("#hiresButton");
+  const openingsButton = d3.select("#openingsButton");
+
+  // Initial selections
+  let selectedIndustry = industryDropdown.property("value");
+  let selectedDataType = hiresButton.classed("selected") ? "Hires" : "JobOpenings";
+
+  // Filter data based on initial selections
+  const filteredData = data.filter(
+    d => d.industry_text === selectedIndustry && d[selectedDataType]
+  );
+
+  // Set up SVG container and scales
+  const margin = { top: 20, right: 100, bottom: 60, left: 80 }; // Increased right margin
+  const width = 800 - margin.left - margin.right; // Increased width
+  const height = 500 - margin.top - margin.bottom; // Increased height
+
+  const svg = d3
+    .select("#plot")
+    .html("") // Clear existing content
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  const xScale = d3
+    .scaleLinear()
+    .domain(d3.extent(filteredData, d => d.year))
+    .range([0, width]);
+
+  const yScale = d3
+    .scaleLinear()
+    .domain([0, d3.max(filteredData, d => d[selectedDataType])])
+    .range([height, 0]);
+
+  // Draw X-axis with all years
+  svg
+    .append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(
+      d3
+        .axisBottom(xScale)
+        .tickFormat(d3.format("d"))
+        .ticks(filteredData.length) // Display all years
+    )
+    .selectAll("text")
+    .style("text-anchor", "end")
+    .attr("dx", "-.8em")
+    .attr("dy", ".15em")
+    .attr("transform", "rotate(-45)");
+
+  // Draw Y-axis with a buffer on the left
+  svg
+    .append("g")
+    .call(d3.axisLeft(yScale).ticks(11));
+
+  // Label for X-axis
+  svg
+    .append("text")
+    .attr("transform", "translate(" + width / 2 + " ," + (height + margin.top + 40) + ")")
+    .style("text-anchor", "middle")
+    .text("Year");
+
+  // Label for Y-axis
+  svg
+    .append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.left)
+    .attr("x", 0 - height / 2)
+    .attr("dy", "1em")
+    .style("text-anchor", "middle")
+    .text(selectedDataType);
+
+  // Draw line and dots
+  const line = d3
+    .line()
+    .x(d => xScale(d.year))
+    .y(d => yScale(d[selectedDataType]));
+
+  svg
+    .append("path")
+    .data([filteredData])
+    .attr("fill", "none")
+    .attr("stroke", "steelblue")
+    .attr("stroke-width", 1.5)
+    .attr("d", line);
+
+  svg
+    .selectAll("dot")
+    .data(filteredData)
+    .enter()
+    .append("circle")
+    .attr("r", 5)
+    .attr("cx", d => xScale(d.year))
+    .attr("cy", d => yScale(d[selectedDataType]));
+
+  // Dropdown change event listener
+  industryDropdown.on("change", function () {
+    selectedIndustry = industryDropdown.property("value");
+    updateChart(data);
+  });
+
+  // Button click event listeners
+  hiresButton.on("click", function () {
+    selectedDataType = "Hires";
+    updateChart(data);
+    updateButtonStyle(this);
+  });
+
+  openingsButton.on("click", function () {
+    selectedDataType = "JobOpenings"; // Ensure correct data type
+    updateChart(data);
+    updateButtonStyle(this);
+  });
+
+  // Initial button styles
+  updateButtonStyle(selectedDataType === "Hires" ? hiresButton.node() : openingsButton.node());
+}
+
+// Function to update button styles
+function updateButtonStyle(selectedButton) {
+  // Remove 'selected' class from all buttons
+  d3.selectAll("button").classed("selected", false);
+
+  // Add 'selected' class to the clicked button
+  d3.select(selectedButton).classed("selected", true);
+}
+
+// Load data and initialize the chart
+d3.csv("https://raw.githubusercontent.com/mohsinchougale/Job-Market-Analysis/main/hires_openings_jolts.csv", rowConverter)
+  .then(function (data) {
+    // Log loaded data to the console
+    console.log(data);
+
+    // Save data globally for easy access
+    window.data = data;
+
+    // Populate dropdown options
+    populateDropdownOptions(data);
+
+    // Initial chart render
+    updateChart(data);
+  })
+  .catch(function (error) {
+    // Handle errors
+    console.error(error);
+  });
